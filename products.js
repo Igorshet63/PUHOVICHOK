fab27e04e583f9cf4a4162d3da6fe1a71e508cb0
Igorshet63 <schetinin6663@gmail.com>
2025-12-26 10:05:09 +0300

    Update products.js

--- products.js
+++ products.js
@@ -6,7 +6,8 @@ const products = [
     { id: 103, category: 'Пальто', group: 'outerwear', name: 'Классическое Пальто ""', price: '7500', img: 'images/photo_palto.jpg', description: 'Двубортное шерстяное пальто с поясом.', characteristics: 'Состав: 70% шерсть, 30% полиэстер. Подкладка: вискоза. Длина: миди. Цвет: бежевый.', sizes: ['S', 'M', 'L', 'XL'] },
     
     // ЛЕДИ (Regular Wear)
-    { id: 201, category: 'Платья', group: 'regular-wear', name: 'Вечернее Платье "Монако"', price: '9 900', img: 'https://placehold.co/400x550/C850C0/FFFFFF?text=Платье', description: 'Длинное атласное платье с открытой спиной.', characteristics: 'Материал: 100% атлас. Цвет: изумрудный. Длина в пол. Скрытая молния.', sizes: ['XS', 'S', 'M'] },
-    { id: 202, category: 'Костюмы', group: 'regular-wear', name: 'Деловой Костюм "Бизнес Леди"', price: '11 200', img: 'https://placehold.co/400x550/C850C0/FFFFFF?text=Костюм', description: 'Стильный брючный костюм из фактурной ткани.', characteristics: 'Материал: 80% вискоза, 20% эластан. Пиджак оверсайз, брюки прямые. Цвет: графит.', sizes: ['S', 'M', 'L'] },
-    { id: 203, category: 'Купальники', group: 'regular-wear', name: 'Купальник "Тропики"', price: '3 200', img: 'https://placehold.co/400x550/C850C0/FFFFFF?text=Купальник', description: 'Раздельный купальник с высокой талией.', characteristics: 'Материал: 85% полиамид, 15% эластан. Быстросохнущая ткань. Защита UV50+.', sizes: ['S', 'M', 'L'] },
+    { id: 201, category: 'Платья', group: 'regular-wear', name: '"Платье"', price: '', img: 'images/photo_plate.jpg', description: '', characteristics: 'Материал: 100% атлас. Цвет: изумрудный. Длина в пол. Скрытая молния.', sizes: ['XS', 'S', 'M'] },
+    { id: 202, category: 'Костюмы', group: 'regular-wear', name: 'Костюм', price: '', img: 'https://images/photo_kostum.jpg', description: 'Летний костюм', characteristics: 'Материал: 80% вискоза, 20% эластан. Пиджак оверсайз, брюки прямые. Цвет: графит.', sizes: ['S', 'M', 'L'] },
+    { id: 203, category: 'Купальники', group: 'regular-wear', name: 'Купальники', price: '', img: 'images/photo_kupalniki.jpg', description: 'купальники в ассортименте.', characteristics: 'Материал: 85% полиамид, 15% эластан. Быстросохнущая ткань. Защита UV50+.', sizes: ['S', 'M', 'L'] },
 ];
+
